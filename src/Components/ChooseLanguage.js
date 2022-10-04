ead26abf9926d592f6fd7fa7cae6da082b3e6aaf
ThaisFReis <thaisfreis@ufrj.br>
2022-10-04 03:12:33 -0300

    Layout sendo alterado

--- src/Components/ChooseLanguage.js
+++ src/Components/ChooseLanguage.js
@@ -3,32 +3,53 @@ import { Link } from "react-router-dom";
 import Typewriter from "typewriter-effect";
 
 import "./ChooseLanguage.css";
+import seta from "../Assets/seta.png";
 
 export default function ChooseLanguage(){
         return(
         <>
+            
             <div className="ChooseLanguage">
+                        {/*
+            
+            <h1>WELCOME TO MY PORTFOLIO</h1>
+            <h2>WELCOME TO MY PORTFOLIO</h2>
+            <h4>WELCOME TO MY PORTFOLIO</h4>
+            <h6>WELCOME TO MY PORTFOLIO</h6>
+            <p>WELCOME TO MY PORTFOLIO</p>
+        */}
                 <div className="welcome">
                     <Typewriter
                     options={{
-                        strings: ["WELCOME TO MY PORTFOLIO", "BEM-VINDO AO MEU PORTFÓLIO"],
+                        strings: ["WELCOME TO MY PORTFOLIO", "BEM-VINDO AO MEU PORTFOLIO"],
                         autoStart: true,
                         loop: true,
                     }}
                     />
                 </div>
                 <div className="text-choose-language">
-                    <p>Choose your language:</p>
+                    <p>Choose a language:</p>
                 </div>
                 <div className="buttons">
-                    <div className="button">
-                        <Link to="/inicio"><p>Portuguese</p></Link>
-                    </div>
-                    <div className="button">
-                        <Link to="/home"><p>English</p></Link>
-                    </div>
+                    <Link to="/inicio">
+                        <div className="btn">
+                            <p>Portuguese</p>
+                            <div className="icon">
+                                <ion-icon name="arrow-forward"></ion-icon>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to="/home">
+                        <div className="btn">
+                            <p>English</p>
+                            <div className="icon">
+                                <ion-icon name="arrow-forward"></ion-icon>
+                            </div>
+                        </div>
+                    </Link>
                 </div>
             </div>
+           
         </>
     )
 }
--- src/Components/Portuguese/Home/Home.js
+++ src/Components/Portuguese/Home/Home.js
@@ -5,16 +5,26 @@ import  "./Home.css"
 
 import Linkedin from "../../../Assets/linkedin.png"
 import Github from "../../../Assets/github.png"
+import Blob from "../../../Assets/blob.svg"
+import BlobPink from "../../../Assets/blob-pink.svg"
+import BlobPink2 from "../../../Assets/blob-pink-2.svg"
+import BlobPink3 from "../../../Assets/blob-pink-3.svg"
+
 
 export default function Home() {
     return (
         <>
         <Menu />
             <div className="Inicio">
+                
                 <div className="principal">
                     <h1>EU SOU</h1>
                     <h2>THAIS FERREIRA REIS</h2>
                     <h1>DESENVOLVEDORA FRONT-END</h1>
+                </div>
+
+                <div className="background">
+                    <img className="blob" src={BlobPink3}/>
                 </div>
 
                 <div className="buttons">
